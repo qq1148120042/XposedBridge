647fcc4b916886a906cab664c2b848288b132bdb
周秀强 <1148120042@qq.com>
2021-01-14 15:23:19 +0800

    ````

---
+++ app/src/main/java/com/android/ks/tool/IXposedMod.java
@@ -0,0 +1,4 @@
+package com.android.ks.tool;
+
+/** Marker interface for Xposed modules. Cannot be implemented directly. */
+/* package */ interface IXposedMod {}
---
+++ app/src/main/java/com/android/ks/tool/callbacks/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains the base classes for callbacks.
+ *
+ * <p>For historical reasons, {@link com.android.ks.tool.XC_MethodHook} and
+ * {@link com.android.ks.tool.XC_MethodReplacement} are directly in the
+ * {@code com.android.ks.tool} package.
+ */
+package com.android.ks.tool.callbacks;
+
